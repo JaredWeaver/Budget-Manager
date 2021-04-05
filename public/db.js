e1a9720427fd16eb643eec56f87bef901e7c57dc
JaredWeav <jared.weaver2@gmail.com>
2021-04-05 13:54:03 -0400

    formatting/offline working

--- public/db.js
+++ public/db.js
@@ -1,9 +1,7 @@
 let db;
-// create a new db request for a "budget" database.
 const request = indexedDB.open('budget', 1);
 
 request.onupgradeneeded = function (event) {
-  // create object store called "pending" and set autoIncrement to true
   const db = event.target.result;
   db.createObjectStore('pending', { autoIncrement: true });
 };
@@ -17,29 +15,19 @@ request.onsuccess = function (event) {
 };
 
 request.onerror = function (event) {
-  // log error here
   console.log(request.error);
-  // console.log("Database error: " + event.target.errorCode)
 };
 
 function saveRecord(record) {
-  // create a transaction on the pending db with readwrite access
   const transaction = db.transaction(['pending'], 'readwrite');
-  // access your pending object store
   const pendingStore = transaction.objectStore('pending');
-  // add record to your store with add method.
   pendingStore.add(record);
-};
+}
 
 function checkDatabase() {
-  // open a transaction on your pending db
   const transaction = db.transaction(['pending'], 'readwrite');
-  // access your pending object store
   const pendingStore = transaction.objectStore('pending');
-  // get all records from store and set to a variable
   const getAll = pendingStore.getAll();
-
-
 
   getAll.onsuccess = function () {
     if (getAll.result.length > 0) {
@@ -53,32 +41,13 @@ function checkDatabase() {
       })
         .then((response) => response.json())
         .then(() => {
-          // if successful, open a transaction on your pending db
           const transaction = db.transaction(['pending'], 'readwrite');
-          // access your pending object store
           console.log(transaction);
           const pendingStore = transaction.objectStore('pending');
-          // clear all items in your store
           pendingStore.clear();
         });
     }
   };
 }
 
-// listen for app coming back online
 window.addEventListener('online', checkDatabase);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
