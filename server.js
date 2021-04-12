45400ac79ba860a027efe6613d6cf3268977c650
JaredWeav <jared.weaver2@gmail.com>
2021-04-12 15:22:21 -0400

    added connection:

--- server.js
+++ server.js
@@ -15,15 +15,16 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+app.use(require("./routes/api.js"));
+
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
   useFindAndModify: false
 });
 
-// routes
-app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
